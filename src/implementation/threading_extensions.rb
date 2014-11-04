80d6e1c68b3a622bd5522548b009b123512e22d3
Isaac Supeene <isupeene@ualberta.ca>
2014-11-03 18:12:59 -0700

    Enabled time limit for the sorting algorithm.

--- src/implementation/threading_extensions.rb
+++ src/implementation/threading_extensions.rb
@@ -1,5 +1,25 @@
+require_relative "cancellation_token"
+
 module ThreadingExtensions
 	def run_parallel(*blocks)
 		blocks.map{ |b| Thread.new{ b.call } }.each{ |t| t.join }
+	end
+
+	def run_with_time_limit(time_limit)
+		source = CancellationTokenSource.new
+
+		if time_limit
+			watchdog = Thread.new {
+				sleep(time_limit)
+				source.cancel
+			}
+			watchdog.priority = 3 # Top priority
+			yield source.token
+			watchdog.kill
+		else
+			yield source.token
+		end
+
+		return !source.token.cancelled # True if the task finished.
 	end
 end
